a89c0e943de5f6d23e75ef8942674b703692f2e8
John Gill <swfiua@gmail.com>
2015-05-17 13:53:33 -0300

    re-organise and remove some customisations.
    desktop-save-mode is putting emacs in weird state with awesome.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,102 +1,8 @@
-;;(setq debug-on-error t)
-;;(setq debug-on-error nil)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(setq package-list
-      '(csharp-mode
-	git-commit-mode
-	git-commit-mode git-rebase-mode git-rebase-mode lua-mode lua-mode
-	magit git-rebase-mode git-commit-mode magit multiple-cursors
-	multiple-cursors python-mode python-mode yasnippet yasnippet
-	autopair bbdb-ext bbdb bbdb-vcard bbdb
-	bbdb2erc bbdb csharp-mode ein-mumamo ein request websocket
-	ipython jedi-direx direx jedi auto-complete popup jedi-core
-	python-environment deferred epc ctable concurrent deferred
-	key-chord lua-mode magit git-rebase-mode git-commit-mode
-	multiple-cursors popup pyflakes python-environment deferred
-	python-mode python-pep8 request twittering-mode w3 w3m websocket
-	))
-
-(package-initialize)
-
-; fetch the list of packages available 
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-
-;; restructured text mode
-
-(require 'rst)
-(require 'twittering-mode)
-(require 'multiple-cursors)
-
-(setq default-frame-alist '(
-			    ;; (font . "Bitstream Vera Sans Mono-9") 
-			    (scroll-bar-mode . right)))
-
-
-;; font-lock mode stuff
-(global-font-lock-mode t)
-(setq font-lock-maximum-decoration
-      '((c-mode . 3) (c++-mode . 3) (tex-mode . 3) (latex-mode . 3)
-	(java-mode . 3)))
-
-;; fix back-ups to copy if links are involved
-(setq backup-by-copying-when-linked t)
-
-;; require final newline when file is saved
-(setq require-final-newline t)
-
-;; pyflakes stuff via flymake
-(when (load "flymake" t) 
-         (defun flymake-pyflakes-init () 
-           (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-                              'flymake-create-temp-inplace)) 
-              (local-file (file-relative-name 
-                           temp-file 
-                           (file-name-directory buffer-file-name)))) 
-             (list "pyflakes" (list local-file)))) 
-
-         (add-to-list 'flymake-allowed-file-name-masks 
-                  '("\\.py\\'" flymake-pyflakes-init)
-                  '("\\.pyx\\'" flymake-pyflakes-init))) 
-
-;; csharp-mode
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-   (setq auto-mode-alist
-      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
-
-;; other goodness
-(require 'hippie-exp)
-
-;; pending-delete mode 
-(require 'delsel)
-(transient-mark-mode t)
-(setq highlight-nonselected-windows nil)
-
-
-(defun my-reverse(&optional arg)
-  (interactive "P")
-  (x-handle-reverse-video (selected-frame) '((reverse . t))))
-
-(my-reverse)
+;; load part 1 of initialisation
+(load "~/.emacs.d/part1.el")
 
 ;; load part 2 of initialisation
 (load "~/.emacs.d/part2.el")
-
-;; bbdb init
-;; (load "~/.emacs.d/bbdbinit.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -108,29 +14,18 @@
  '(browse-url-browser-function (quote browse-url-xdg-open))
  '(browse-url-firefox-startup-arguments (quote ("")))
  '(case-fold-search t)
- '(clean-buffer-list-delay-general 4)
- '(current-language-environment "Latin-1")
- '(default-input-method "latin-1-prefix")
  '(delete-selection-mode t nil (delsel))
  '(desktop-save-mode t)
  '(dired-recursive-deletes t)
  '(display-time-mode t)
  '(flymake-allowed-file-name-masks (quote (("\\.py\\'" flymake-pyflakes-init) ("\\.xml\\'" flymake-xml-init) ("\\.html?\\'" flymake-xml-init) ("\\.cs\\'" flymake-simple-make-init) ("\\.p[ml]\\'" flymake-perl-init) ("\\.php[345]?\\'" flymake-php-init) ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup) ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) ("\\.idl\\'" flymake-simple-make-init))))
  '(global-font-lock-mode t nil (font-lock))
- '(jabber-connection-type (quote ssl))
- '(jabber-network-server "localhost")
- '(jabber-server "gmail.com")
- '(jabber-username "swfiua")
  '(latex-run-command "pdflatex")
- '(lpr-page-header-switches (quote ("-l 66")))
  '(midnight-mode t nil (midnight))
  '(mm-text-html-renderer (quote w3m))
- '(msb-mode t nil (msb))
  '(org-agenda-files (quote ("~/personal/hols.org" "~/todo.org" "~/Devel/TODO/travel.org")))
  '(py-indent-offset 4)
  '(scroll-bar-mode (quote right))
- '(speedbar-activity-change-focus-flag t)
- '(speedbar-track-mouse-flag nil)
  '(tool-bar-mode nil nil (tool-bar))
  '(tooltip-mode nil nil (tooltip))
  '(vc-handled-backends (quote (Bzr RCS CVS SVN SCCS Git Hg Mtn Arch))))
@@ -142,3 +37,7 @@
  ;; If there is more than one, they won't work right.
  '(rst-level-1-face ((t (:background "grey40"))) t)
  '(rst-level-2-face ((t (:background "grey40"))) t))
+
+
+
+
---
+++ .emacs.d/part1.el
@@ -0,0 +1,97 @@
+;;(setq debug-on-error t)
+;;(setq debug-on-error nil)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(setq package-list
+      '(csharp-mode
+	git-commit-mode
+	git-commit-mode git-rebase-mode git-rebase-mode lua-mode lua-mode
+	magit git-rebase-mode git-commit-mode magit multiple-cursors
+	multiple-cursors python-mode python-mode yasnippet yasnippet
+	autopair bbdb-ext bbdb bbdb-vcard bbdb
+	bbdb2erc bbdb csharp-mode ein-mumamo ein request websocket
+	ipython jedi-direx direx jedi auto-complete popup jedi-core
+	python-environment deferred epc ctable concurrent deferred
+	key-chord lua-mode magit git-rebase-mode git-commit-mode
+	multiple-cursors popup pyflakes python-environment deferred
+	python-mode python-pep8 request twittering-mode w3 w3m websocket
+	))
+
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;; restructured text mode
+
+(require 'rst)
+(require 'twittering-mode)
+(require 'multiple-cursors)
+
+(setq default-frame-alist '(
+			    ;; (font . "Bitstream Vera Sans Mono-9") 
+			    (scroll-bar-mode . right)))
+
+
+;; font-lock mode stuff
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration
+      '((c-mode . 3) (c++-mode . 3) (tex-mode . 3) (latex-mode . 3)
+	(java-mode . 3)))
+
+;; fix back-ups to copy if links are involved
+(setq backup-by-copying-when-linked t)
+
+;; require final newline when file is saved
+(setq require-final-newline t)
+
+;; pyflakes stuff via flymake
+(when (load "flymake" t) 
+         (defun flymake-pyflakes-init () 
+           (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                              'flymake-create-temp-inplace)) 
+              (local-file (file-relative-name 
+                           temp-file 
+                           (file-name-directory buffer-file-name)))) 
+             (list "pyflakes" (list local-file)))) 
+
+         (add-to-list 'flymake-allowed-file-name-masks 
+                  '("\\.py\\'" flymake-pyflakes-init)
+                  '("\\.pyx\\'" flymake-pyflakes-init))) 
+
+;; csharp-mode
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+   (setq auto-mode-alist
+      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+
+;; other goodness
+(require 'hippie-exp)
+
+;; pending-delete mode 
+(require 'delsel)
+(transient-mark-mode t)
+(setq highlight-nonselected-windows nil)
+
+
+(defun my-reverse(&optional arg)
+  (interactive "P")
+  (x-handle-reverse-video (selected-frame) '((reverse . t))))
+
+(my-reverse)
+
+;; bbdb init
+;; (load "~/.emacs.d/bbdbinit.el")
+
