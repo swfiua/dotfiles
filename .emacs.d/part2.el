69b8d68e34e9bcc06606f20124488b3c15904613
Johnny Gill <swfiua@gmail.com>
2017-02-27 11:22:10 -0400

    turn off ido-mode and dired-omit mode

--- .emacs.d/part2.el
+++ .emacs.d/part2.el
@@ -170,11 +170,11 @@
 (server-start)
 
 ;; turn on iswitchb-mode
-(ido-mode)
+;;(ido-mode)
 
 ;; dired-x for omit mode
 (add-hook 'dired-load-hook '(lambda () (require 'dired-x))) ; Load Dired X when Dired is loaded.
-(setq dired-omit-mode t) ; Turn on Omit mode.
+;;(setq dired-omit-mode t) ; Turn on Omit mode.
 
 ;; have l work as go back in help mode
 (add-hook 'help-mode-hook
