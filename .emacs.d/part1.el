bd5cd8ffbd4ce429a132de93ed8bf58268aade6e
Johnny Gill <swfiua@gmail.com>
2020-05-06 15:43:36 -0400

    remove details from .emacs

--- .emacs.d/part1.el
+++ .emacs.d/part1.el
@@ -1,30 +1,6 @@
 ;;(setq debug-on-error t)
 ;;(setq debug-on-error nil)
 
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(setq package-list '(git-commit 
-      lua-mode magit gh
-      python-mode bbdb-ext bbdb bbdb-vcard bbdb
-      bbdb2erc  ein-mumamo ein request websocket
-      direx popup python-environment deferred epc ctable
-      concurrent w3 w3m))
-
-(package-initialize)
-
-;; fetch the list of packages available 
-(unless package-archive-contents
-  (package-refresh-contents))
- 
-;; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
 
 ;; load theme
 (load-theme 'deeper-blue)
@@ -53,24 +29,6 @@
 ;; require final newline when file is saved
 (setq require-final-newline t)
 
-;; pyflakes stuff via flymake
-(when (load "flymake" t) 
-         (defun flymake-pyflakes-init () 
-           (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-                              'flymake-create-temp-inplace)) 
-              (local-file (file-relative-name 
-                           temp-file 
-                           (file-name-directory buffer-file-name)))) 
-             (list "pyflakes" (list local-file)))) 
-
-         (add-to-list 'flymake-allowed-file-name-masks 
-                  '("\\.py\\'" flymake-pyflakes-init)
-                  '("\\.pyx\\'" flymake-pyflakes-init))) 
-
-;; csharp-mode
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-   (setq auto-mode-alist
-      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
 ;; other goodness
 (require 'hippie-exp)
@@ -87,10 +45,6 @@
 
 ;;(if (display-graphic-p)
 ;;    (my-reverse))
-
-(defun details()
-  (interactive)
-  (setq user-mail-address "john@details.bm"))
 
 (defun swfiua()
   (interactive)
